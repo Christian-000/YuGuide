c2b5df8a8cdd516ca322145ac597ba23c42e9116
Christian-000 <christianpiccinini4@gmail.com>
2022-03-02 19:00:16 -0300

    yay

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
 import React from "react";
-import { BrowserRouter,Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import LandingPage from "./components/Landing/LandingPage";
 import Home from "./components/Home/Home";
 import CreateDeck from "./components/CreateDeck/CreateDeck";
 import CardDetail from "./components/CardDetail/CardDetail";
 
 function App() {
-  const landing = new LandingPage();
-  const home = new Home();
-  const deck = new CreateDeck();
-  const cardDetail = new CardDetail();
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={landing} />
-      <Route path="/home/*" element={home} />
-      <Route path="/home/createDeck" element={deck} />
-      <Route path="/cardDetail/:id" element={cardDetail} />
-      <Route path="*" element={<h1> 404 NOT FOUND</h1>}/>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/home/*" element={<Home />} />
+      <Route path="/home/createDeck" element={<CreateDeck />} />
+      <Route path="/cardDetail/:id" element={<CardDetail />} />
+      <Route path="*" element={<h1> 404 NOT FOUND</h1>}></Route>
     </Routes>
     </BrowserRouter>
+
   );
 }
 
