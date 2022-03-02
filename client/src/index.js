2fac4e3227c16a4dfc5d53e3b5298a8ec46c7da9
Christian-000 <christianpiccinini4@gmail.com>
2022-03-02 18:16:40 -0300

    andy commit

--- client/src/index.js
+++ client/src/index.js
@@ -7,9 +7,7 @@ import { Provider } from "react-redux";
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <BrowserRouter>
         <App />
-      </BrowserRouter>
     </React.StrictMode>
   </Provider>,
   document.getElementById("root")
